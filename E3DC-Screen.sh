bf6018def15fd39b5c0bd32b150eacc7ee20b9e9
chrkulbe <ckulbe@gmx.net>
2021-08-31 19:20:33 +0200

    detect dead screen and restart

--- E3DC-Screen.sh
+++ E3DC-Screen.sh
@@ -7,12 +7,16 @@ echo "on Segmentation fault check path and access of logifle (uid 1111/gid 1111)
 # this is never ending
 while true
 do
+    if (screen -list | grep "(Dead ???)" >/dev/null)
+        screen "wipe out dead screen"
+        screen -wipe
+    fi
     # check if screen died and restart it
     if ! (screen -list | grep "E3DC" >/dev/null)
     then
-        echo E3DC-Control wird gestartet...
+        echo "E3DC-Control wird gestartet..."
         ash -c "screen -dmS E3DC ./E3DC.sh"
-        echo E3DC-Control gestartet.
+        echo "E3DC-Control gestartet."
     fi
     sleep 10
 done
