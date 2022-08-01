809ee8d91a6edfaeb2b77dac912fe1553f60b301
ChrisKuBa <chriskuba@mail.de>
2022-08-01 08:21:08 +0200

    start script send lighttpd to background - does not block anymore; fix build scripts

--- build.cmd
+++ build.cmd
@@ -1,5 +1,9 @@
+@echo off
+set DATE=%date:~6,4%-%date:~3,2%-%date:~0,2%T%time:~0,2%:%time:~3,2%:%time:~6,2%+01:00
+set DATE=%DATE: =0%
+
 docker build ^
   --no-cache=true ^
-  --build-arg BUILD_DATE="%date:~6,4%-%date:~3,2%-%date:~0,2%T%time:~0,2%:%time:~3,2%:%time:~6,2%+01:00" ^
+  --build-arg BUILD_DATE=%DATE% ^
   -t e3dc-control ^
   .
--- buildx.cmd
+++ buildx.cmd
@@ -1,6 +1,8 @@
 @echo off
 
 git log -1 --format=%%H > tmpFile
+set DATE=%date:~6,4%-%date:~3,2%-%date:~0,2%T%time:~0,2%:%time:~3,2%:%time:~6,2%+01:00
+set DATE=%DATE: =0%
 set /p REVISION= < tmpFile
 del tmpFile 
 FOR /F "tokens=*" %%g IN ('git describe --tags') do (SET TAG=%%g)
@@ -8,7 +10,7 @@ FOR /F "tokens=*" %%g IN ('git describe --tags') do (SET TAG=%%g)
 docker buildx build ^
   --platform linux/amd64,linux/arm64,linux/arm/v7 ^
   --no-cache=true ^
-  --build-arg BUILD_DATE=%date:~6,4%-%date:~3,2%-%date:~0,2%T%time:~0,2%:%time:~3,2%:%time:~6,2%+01:00 ^
+  --build-arg BUILD_DATE=%DATE% ^
   --build-arg BUILD_VERSION=%TAG% ^
   --build-arg BUILD_REVISION=%REVISION% ^
   -t chriskuba/e3dc-control:%TAG% ^
